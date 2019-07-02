a4097950fdd3827d918fcccf79b59fe6c29970d2
Irene Lam <ilam1@stuy.edu>
2019-07-01 22:28:01 -0400

    push to github

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   exports: [
     MatToolbarModule,
     MatButtonModule,
+    MatGridListModule,
   ]
 })
 export class MaterialModule { }
