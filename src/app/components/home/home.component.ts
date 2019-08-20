24121ff9b92de1e42e806f8d817b42d5e2dca111
Irene Lam <ilam1@stuy.edu>
2019-08-20 01:12:04 -0400

    Fixed home page animations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+// import Typed from 'typed.js';
+
+// var options = {
+//   strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+//   typeSpeed: 40
+// }
+
+// var typed = new Typed(".element", options);
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
